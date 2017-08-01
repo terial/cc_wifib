77364f58580fbac7b36f38270936183815f0c42f
terial <nkariger@gmail.com>
2017-07-31 17:40:34 -0700

    Update install_wifib.sh

--- install_wifib.sh
+++ install_wifib.sh
@@ -11,6 +11,11 @@ fi
 set -e
 set -x
 
+# Install from directory
+DIR_INSTALL=$(pwd)
+# Install to directory
+DIR_INSTALL_WIFIBROADCAST=/usr/local/bin/wifibroadcast
+
 # Get updated and install packages
 echo "Updating repository and installing required packages..."
 sudo apt-get update
@@ -35,8 +40,36 @@ make install
 echo "Cloning wifibroadcast repository and build,make,install..."
 cd  ~
 git clone https://github.com/terial/cc_wifib.git
-cd config
-cp apconfig.txt /boot/apconfig.txt
-cp joyconfig.txt /boot/joyconfig.txt
-cp osdconfig.txt /boot/osdconfig.txt
-cp wifibroadcast-1.txt /boot/wifibroadcast-1.txt
+
+# Copy configuration text files to /boot
+cp $DIR_INSTALL/config/apconfig.txt /boot/apconfig.txt
+cp $DIR_INSTALL/config/joyconfig.txt /boot/joyconfig.txt
+cp $DIR_INSTALL/config/osdconfig.txt /boot/osdconfig.txt
+cp $DIR_INSTALL/config/wifibroadcast-1.txt /boot/wifibroadcast-1.txt
+
+# Copy wifibroadcast files to DIR_INSTALL_WIFIBROADCAST
+cp -a $DIR_INSTALL/wifibroadcast/. DIR_INSTALL_WIFIBROADCAST/wifibroadcast
+cp -a $DIR_INSTALL/wifibroadcast_misc/. DIR_INSTALL_WIFIBROADCAST/wifibroadcast_misc
+cp -a $DIR_INSTALL/wifibroadcast_osd/. DIR_INSTALL_WIFIBROADCAST/wifibroadcast_osd
+cp -a $DIR_INSTALL/wifibroadcast_rc/. DIR_INSTALL_WIFIBROADCAST/wifibroadcast_rc
+cp -a $DIR_INSTALL/wifibroadcast_status/. DIR_INSTALL_WIFIBROADCAST/wifibroadcast_status
+
+# Using dos2unix, copy the configuration files from /boot and rename as needed
+# using flag -n will always write to a new file.
+dos2unix -n /boot/osdconfig.txt DIR_INSTALL_WIFIBROADCAST/wifibroadcast_osd/osdconfig.h
+dos2unix -n /boot/joyconfig.txt DIR_INSTALL_WIFIBROADCAST/wifibroadcast_rc/rctx.h > /dev/null 2>&1
+#dos2unix -n /boot/apconfig.txt /tmp/apconfig.txt # unused until ap is setup
+
+make all
+cd ..
+cd cd wifibroadcast_osd
+make all
+cd ..
+cd wifibroadcast_rc
+chmod 755 build.sh
+./build.sh
+cd..
+cd wifibroadcast_status
+make all
+
+
