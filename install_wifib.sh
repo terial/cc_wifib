849f244926662828d448f278d77ea6716d670500
terial <nkariger@gmail.com>
2017-07-31 17:19:48 -0700

    Update install_wifib.sh

--- install_wifib.sh
+++ install_wifib.sh
@@ -12,6 +12,7 @@ set -e
 set -x
 
 # Get updated and install packages
+echo "Updating repository and installing required packages..."
 sudo apt-get update
 sudo apt-get install -y git
 sudo apt-get install -y libpcap0.8-dev # For wifibroadcast core 
@@ -22,9 +23,20 @@ sudo apt-get install -y libsdl2-dev # For WifiBroadcast_rc
 sudo apt-get install -y dos2unix # convert text files from /boot from dos format to unix
 
 # Build and Install OpenVG for wifibroadcast_osd
+echo "Cloning OpenVG repository and build,make,install..."
 cd ~
 sudo git clone https://github.com/ajstarks/openvg.git # OpendVG for raspberry pi
 cd openvg
 make all
 make library
 make install
+
+# Clone repository
+echo "Cloning wifibroadcast repository and build,make,install..."
+cd  ~
+git clone https://github.com/terial/cc_wifib.git
+cd config
+cp apconfig.txt /boot/apconfig.txt
+cp joyconfig.txt /boot/joyconfig.txt
+cp osdconfig.txt /boot/osdconfig.txt
+cp wifibroadcast-1.txt /boot/wifibroadcast-1.txt
